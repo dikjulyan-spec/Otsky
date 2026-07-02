291fab94fc8cb60af7c246378eb3f7276e36077c
dikjulyan-spec <dik.julyan@gmail.com>
2026-07-03 01:35:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.OTSKY_CMS_CONFIG = {
+  // Isi dengan URL Web App Google Apps Script yang berakhiran /exec
+  apiUrl: "",
+
+  // Dipakai kalau apiUrl belum diisi atau GSheet gagal dibaca
+  fallbackContentUrl: "content.json",
+
+  // Timeout load data dari GSheet
+  loadTimeoutMs: 12000
+};
